4df700ce7de837841ba3102ac580ef0ffe6b6be1
fotoz <fotozxx@gmail.com>
2021-11-03 09:43:23 +0200

    header

--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,11 @@ $(function () {
 
 //===== User scripts:
 
-//===== Smooth scroll:
-$('a[href^="#"]:not([href="#"])').on('click', function(event) {
-  event.preventDefault();
+@@include('scripts/smooth-scroll.js')
 
-  $('html,body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  }, 350);
-});
+@@include('scripts/touch.js')
 
+@@include('scripts/mmenu.js')
 
-//===== Touch event check:
-if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-  document.body.classList.add('touch');
-} else {
-  document.body.classList.add('no-touch');
-}
-
+@@include('scripts/dynamic-adapt.js')
 });
